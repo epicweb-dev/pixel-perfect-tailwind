18fe058bbf307da27fe34fb5de9423d297df33bb
Simon Vrachliotis <simon@simonswiss.com>
2024-02-22 12:31:14 +1100

    Fix prettier config to make Tailwind prettier plugin using the correct Tailwind config for each project?

--- tailwind.config.ts
+++
@@ -1,3 +0,0 @@
-/* 
-This file needs to be here as a "placeholder", because we set the path to the config file in the root `.prettierrc` file, so that `prettier-plugin-tailwindcss` works with the correct config file in every exercise app.
-*/
