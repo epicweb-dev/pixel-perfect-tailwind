09c08e687e0f7bf215611db7df4c2c7106e3cd16
Simon Vrachliotis <simon@simonswiss.com>
2024-03-07 13:37:23 +1100

    Step 3 💪

---
+++ exercises/07.upgrade-to-tailwind-v4/03.problem.font-sizes/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), tailwindcss()],
+	server: {
+		port: Number(process.env.PORT) || 3000,
+	},
+})
