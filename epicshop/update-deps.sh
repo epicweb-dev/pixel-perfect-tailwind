1dae3cf1867814ba75b3629b41e70c5b236cb1d6
Kent C. Dodds <me+github@kentcdodds.com>
2024-06-27 07:41:36 -0600

    fix deploy install

--- epicshop/update-deps.sh
+++ epicshop/update-deps.sh
@@ -1,4 +1,4 @@
-npx npm-check-updates --dep prod,dev --upgrade --workspaces --root
+npx npm-check-updates --dep prod,dev --upgrade --root
 cd epicshop && npx npm-check-updates --dep prod,dev --upgrade --root
 cd ..
 rm -rf node_modules package-lock.json ./epicshop/package-lock.json ./epicshop/node_modules ./exercises/**/node_modules
